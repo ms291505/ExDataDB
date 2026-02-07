a23d4e5130b94451e4003d96c8a442af246148a5
ms291505 <clang-anchor.0n@icloud.com>
2026-02-07 10:27:18 -0500

    quick fix

--- client/src/Components/QueryClientProvider.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-type Props = {
-  children: ReactNode
-}
-
-const queryClient = new QueryClient();
-
-export default function QueryClientProvider({ children }: Props)
